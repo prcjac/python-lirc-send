822b8d1e9be37582a35ed4fd62ccda4d4ffd1374
Peter Cowan <prc@corefiling.com>
2015-03-22 17:30:59 +0000

    Refactor into a usable object

--- lirc-send.py
+++ lirc-send.py
@@ -23,26 +23,70 @@ extended. Sadly lirc 0.9.2  isn't yet available for Raspian so
 falling back to raw socket communication... delicious.
 """
 import socket
+from threading import Lock
 
 ENCODING = 'utf-8'
 STRING_BUFFER_LEN = 256
+DEFAULT_UNIX_SOCKET = "/var/run/lirc/lircd"
+LOCAL_HOST_IP = "127.0.0.1"
+DEFAULT_LIRC_PORT = 8765
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("10.0.2.98", 8765))
+class LircSend:
+	"""Use the class methods instead"""
+	_s = None
+	_init = False
+	_lock = Lock()
+	
+	def __init__(self, location = None, port = None):
+		if not port:
+			self._s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+			self._s.connect(DEFAULT_UNIX_SOCKET)
+		else:
+			self._s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self._s.connect((location, port))
+		self._init = True
+	
+	def destroy(self):
+		try:
+			self._lock.acquire()
+			self._s.close()
+			self._s = None
+			self._init = False
+		finally:
+			self._lock.release()
+	
+	@classmethod
+	def create_local(cls, location = None):
+		if not location:
+			location = DEFAULT_UNIX_SOCKET
+		return cls(location)
 
-def send_packet(command):
-	message = "SEND_ONCE LED_24_KEY %s\n" % command
-	s.sendall(message)
-	buf = ""
-	while not buf.endswith("END\n"):
-		data = s.recv(256)
-		buf += data
-		print data
+	@classmethod
+	def create_remote(cls, location = None, port = None):
+		if not location:
+			location = LOCAL_HOST_IP
+		if not port:
+			port = DEFAULT_LIRC_PORT
+		return cls(location, port)
 
-print "start"
+	def send_packet(self, command):
+		if not self._init:
+			raise ValueError("Object has been destroyed")
+		try:
+			self._lock.acquire()
+			message = "SEND_ONCE LED_24_KEY %s\n" % command
+			self._s.sendall(message)
+			buf = ""
+			while not buf.endswith("END\n"):
+				data = self._s.recv(256)
+				buf += data
+				print data
+		finally:
+			self._lock.release()
 
-send_packet("ON")
-send_packet("RED")
-send_packet("OFF")
-s.close()
 
+lirc = LircSend.create_remote("10.0.2.98")
+lirc.send_packet("ON")
+lirc.send_packet("RED")
+lirc.send_packet("OFF")
+lirc.destroy()
